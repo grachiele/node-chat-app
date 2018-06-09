77b0bfd491b012b3f8b5cca853498eb969220388
Gianpaul Rachiele <gianpaul817@gmail.com>
2018-06-09 12:53:17 -0400

    added server changes:

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
 const publicPath = path.join(__dirname, "../public");
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 const port = process.env.PORT || 3000
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log("Disconnected from client");
+  })
 });
 
-console.log(__dirname + "/../public");
-console.log(publicPath);
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
