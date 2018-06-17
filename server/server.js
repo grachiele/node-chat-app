04e0003dce8cec8e7d66e5f6985d254d2e99baf7
Gianpaul Rachiele <gianpaul817@gmail.com>
2018-06-17 17:06:31 -0400

    added new broadcast method to broadcast to all other users besides the current user

--- server/server.js
+++ server/server.js
@@ -17,11 +17,28 @@ io.on('connection', (socket) => {
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+
+    socket.emit('newMessage', {
+      from: 'Admin',
+      text: 'Welcome to the chat app'
+    })
+
+    socket.broadcast.emit('newMessage', {
+      from: "Admin",
+      text: "New User has joined",
+      createdAt: new Date().getTime()
+    });
+
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
 
