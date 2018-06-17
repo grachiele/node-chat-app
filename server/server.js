45ecd24d331cbb01be75cbffbfd4487ae031cbd2
Gianpaul Rachiele <gianpaul817@gmail.com>
2018-06-17 17:57:33 -0400

    refactoring

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
+const {generateMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, "../public");
 const app = express();
@@ -18,23 +19,11 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
 
-    socket.emit('newMessage', {
-      from: 'Admin',
-      text: 'Welcome to the chat app',
-      createdAt: new Date().getTime()
-    })
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-    socket.broadcast.emit('newMessage', {
-      from: "Admin",
-      text: "New User has joined",
-      createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User has joined'));
 
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -4,7 +4,9 @@ var {generateMessage} = require('./message')
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
-    let returned = generateMessage("Adam", "This is a test");
+    let from = "Adam";
+    let text = "This is a test";
+    let returned = generateMessage(from, text);
     expect(returned.from).toEqual('Adam');
     expect(returned.text).toEqual('This is a test');
     expect(typeof returned.createdAt).toEqual('number');
